e12ce8092baba54b25e7b10def12f6e4967bb800
Melih Yaşar SÖZEN <mlhysrszn@melihs-air.local>
2024-02-25 23:12:39 +0300

    Initial Commit

---
+++ Reservy/ReservyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReservyApp.swift
+//  Reservy
+//
+//  Created by Melih Yaşar SÖZEN on 25.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ReservyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
