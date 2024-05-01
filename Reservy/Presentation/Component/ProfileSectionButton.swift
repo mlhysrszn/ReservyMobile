ca965917d0a4c7cbe80678a1c8cf683348381eaf
Melih Yaşar SÖZEN <mlhysrszn@melihs-air.local>
2024-05-01 21:36:30 +0300

    add: profile screen

---
+++ Reservy/Presentation/Component/ProfileSectionButton.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileSectionButton.swift
+//  Reservy
+//
+//  Created by Melih Yaşar SÖZEN on 30.04.2024.
+//
+
+import SwiftUI
+
+struct ProfileSectionButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileSectionButton()
+}
